1a0e4782743e72b1997db405d3d406ec32b418d2
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2025-01-25 00:44:47 -0500

    Update projects.js

--- libs/projects.js
+++ libs/projects.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.module.js";
+import THREE from "https://cdn.jsdelivr.net/npm/three@0.132.2/build/three.module.js";
 import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.132.2/examples/jsm/loaders/GLTFLoader.js";
 
 export const projects = [
